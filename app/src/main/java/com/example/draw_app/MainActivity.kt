0bc13b566586c5fb4f8315a6c340ccbac3f8aab0
saitohakira <62328990+saitohakira@users.noreply.github.com>
2021-03-03 16:23:57 +0700

    add save image

--- app/src/main/java/com/example/draw_app/MainActivity.kt
+++ app/src/main/java/com/example/draw_app/MainActivity.kt
@@ -4,6 +4,10 @@ import android.app.Activity
 import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Color
+import android.os.AsyncTask
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
@@ -15,7 +19,9 @@ import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
-import java.lang.Exception
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     private var mImageButtonCurrentPaint: ImageButton? = null
@@ -36,34 +42,42 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeChosserDialog()
         }
         ib_images.setOnClickListener {
-            if(isReadStorageAllow()){
+            if (isReadStorageAllow()) {
                 //run
-                val pickPhoto = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                val pickPhoto =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                 startActivityForResult(pickPhoto, GALLERY)
-            } else{
+            } else {
                 requestStoragePermission()
             }
         }
         ib_undo.setOnClickListener {
             drawing_view.onClickUndo()
         }
+        ib_save.setOnClickListener {
+            if (isReadStorageAllow()) {
+                //run
+                BitmapAsyncTask(getBitMapFromView(drawing_view_container)).execute()
+            } else {
+                requestStoragePermission()
+            }
+        }
 
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if(resultCode == Activity.RESULT_OK){
-            if(requestCode == GALLERY){
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
                 try {
-                    if(data!!.data != null){
+                    if (data!!.data != null) {
                         iv_background.visibility = View.VISIBLE
                         iv_background.setImageURI(data.data)
                     } else {
-                        Toast.makeText(this, "Failed" , Toast.LENGTH_LONG).show()
+                        Toast.makeText(this, "Failed", Toast.LENGTH_LONG).show()
 
                     }
-                }
-                catch (e: Exception){
+                } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
@@ -126,15 +140,16 @@ class MainActivity : AppCompatActivity() {
                     android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 ).toString()
             )
-        ){
+        ) {
 
         }
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(
-                    android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                    android.Manifest.permission.WRITE_EXTERNAL_STORAGE,), STORAGE_PERMISSION_CODE
-            )
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            ), STORAGE_PERMISSION_CODE
+        )
     }
 
     override fun onRequestPermissionsResult(
@@ -143,18 +158,67 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if(requestCode == STORAGE_PERMISSION_CODE){
-            if(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(this, "No permission" , Toast.LENGTH_LONG).show()
-            }
-            else {
-                Toast.makeText(this, "No permission" , Toast.LENGTH_LONG).show()
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "No permission", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "No permission", Toast.LENGTH_LONG).show()
             }
         }
     }
+
     private fun isReadStorageAllow(): Boolean {
-        val result = ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+        val result = ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        )
         return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun getBitMapFromView(view: View): Bitmap {
+        val returnBitmap = Bitmap.createBitmap(view.width, view.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(returnBitmap)
+        val bgDrawable = view.background
+        if (bgDrawable != null) {
+            bgDrawable.draw(canvas)
+
+        } else {
+            canvas.drawColor(Color.WHITE)
+        }
+        view.draw(canvas)
+        return returnBitmap
+    }
+
+    private inner class BitmapAsyncTask(val bitmap: Bitmap) : AsyncTask<Any, Void, String>() {
+        override fun doInBackground(vararg params: Any?): String {
+            var result = ""
+            if (bitmap != null) {
+                try {
+                    val bytes = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 99, bytes)
+                    val f = File(externalCacheDir!!.absoluteFile.toString() + File.separator + "painting" + System.currentTimeMillis() / 100 + ".png")
+                    val fo = FileOutputStream(f)
+                    fo.write(bytes.toByteArray())
+                    fo.close()
+                    result = f.absolutePath
+                } catch (e: Exception) {
+                    result = ""
+                    e.printStackTrace()
+                }
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            if(result!!.isNotEmpty()){
+                Toast.makeText(this@MainActivity, "Save Success ${result}", Toast.LENGTH_LONG).show()
+            } else  {
+                Toast.makeText(this@MainActivity, "Something went wrong while saving file", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
 
     companion object {
